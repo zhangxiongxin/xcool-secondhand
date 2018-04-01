1c5fdd31cb5383f20448d0abb019b5dcda2e2a34
zhangxiongxin <986192524@qq.com>
2018-04-01 13:42:43 +0800

    home page

---
+++ front/src/components/card.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  el-row.card-container(:gutter="20")
+    el-col.simgle(v-for="(el, index) in item", @click="fun(el.id)",:key="index")
+      h1 {{ el.name }}
+</template>
+<script>
+export default {
+  name: 'card',
+  props: ['item', 'index'],
+  methods: {
+    fun (id) {
+      console.log(id)
+    }
+  }
+}
+</script>
+<style scoped>
+  .simgle {
+    float: left;
+    width: 10rem;
+    height: 14rem;
+    border: 1px solid #000;
+    padding: 10px;
+  }
+</style>
--- front/src/pages/home/index.vue
+++ front/src/pages/home/index.vue
@@ -1,25 +1,73 @@
 <template lang="pug">
   .home-index-container
-    .home-head
+    .home-head.w
       .home-head-left
         a.login(@click="to('/login')") 亲，请登录
         a.register(@click="to('/register')") 免费注册
       //- upload
     .home-logo
-    .home-banner
-    .home-list
+      .content.w
+        .name
+          .name-img 标题
+        el-menu.meui(mode="horizontal", background-color="#ffda44", text-color="#000")
+          el-menu-item.item(index="it") 数码
+          el-menu-item.item(index="cl") 文娱
+          el-menu-item.item(index="daily") 日用
+          el-menu-item.item(index="vual") 虚拟
+    .home-banner.w
+      .brast 发布
+      el-carousel.banner(:interval="5000" arrow="always")
+        el-carousel-item(v-for="item in 4", :key="item")
+          h3 {{ item }}
+    .home-list.w
+      card(:item="goodsList")
     .home-bottom
 </template>
 <script>
-import upload from '../../components/upload'
+import upload from '@/components/upload'
+import card from '@/components/card'
 export default {
   data () {
     return {
-
+      goodsList: [
+        {
+          name: '篮球',
+          id: 'aaa'
+        },
+        {
+          name: '女朋友',
+          id: 'bbb'
+        },
+        {
+          name: '篮球',
+          id: 'aaa'
+        },
+        {
+          name: '女朋友',
+          id: 'bbb'
+        },
+        {
+          name: '篮球',
+          id: 'aaa'
+        },
+        {
+          name: '女朋友',
+          id: 'bbb'
+        },
+        {
+          name: '篮球',
+          id: 'aaa'
+        },
+        {
+          name: '女朋友',
+          id: 'bbb'
+        }
+      ]
     }
   },
   components: {
-    upload: upload
+    upload: upload,
+    card: card
   },
   methods: {
     to (url) {
@@ -29,6 +77,11 @@ export default {
 }
 </script>
 <style scoped>
+  .w {
+    max-width: 1200px;
+    min-width: 900px;
+    margin: 0 auto;
+  }
   .home-head-left {
     line-height: 40px;
     font-size: 12px;
@@ -39,12 +92,68 @@ export default {
     margin-right: 7px;
   }
   .home-head {
-    max-width: 1200px;
     height: 40px;
-    margin: 0 auto;
   }
   .home-logo {
     height: 90px;
     background: #ffda44;
+  }
+  .content {
+    display: grid;
+    grid-template-columns: 35% auto;
+    grid-template-rows: auto;
+    grid-column-gap: 3rem;
+  }
+  .name {
+    text-align: left;
+    font-size: 3rem;
+    line-height: 90px;
+    position: relative;
+  }
+  .name-img::after {
+    display: inline-block;
+    content: '让闲置游荡起来！';
+    font-size: .8rem;
+    position: absolute;
+    top: 0;
+    left: 6.5rem;
+  }
+  .name-slogan {
+    font-size: .8rem;
+  }
+  .item {
+    height: 90px;
+    line-height: 90px;
+    font-size: 1rem;
+  }
+
+  .home-banner {
+    margin-top: 20px;
+    height: 300px;
+    display: grid;
+    grid-template-columns: 30% auto;
+    grid-column-gap: 10px;
+  }
+  .banner {
+    height: 100%;
+  }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  .brast {
+    border: 1px solid #000;
+  }
+  .home-list {
+    margin-top: 20px;
   }
 </style>
